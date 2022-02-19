327c4d2d5e2cf4e8b08d544a9166edca1a857c46
Markus Risa Vesetrud <markusrv@stud.ntnu.no>
2022-02-19 16:01:28 +0100

    improve commentary

--- Python Kurs/src/basic_python/6_what_if.py
+++ Python Kurs/src/basic_python/6_what_if.py
@@ -1,5 +1,10 @@
 # I en if else setning vil alltid nøyaktig en av printene kjøre
 # Hvis man bare har if og elif kan det hende ingen kjører
+
+# Pass på forskjellen mellom = og ==
+# = betyr sett variabelen til venstre lik verdien til høyre
+# == betyr sjekk om verdiene til høyre og venstre er like
+
 print(6 * 7 < 42)
 print(6 * 7 == 42)
 print(6 * 7 > 42)
@@ -13,9 +18,8 @@ else:
 
 # Legg merke til at når man har kolon ":" i python, så forventes det at man hopper inn et hakk på neste linje
 # Alt som ligger på samme nivå eller mer til høyre inntil man hopper tilbake er en del av samme blokk
-
 # For eksempel:
-# Alle linjene fra og med 17 til og med 21 er en del av blokken som kjøres hvis 3 < 7
+# Alle linjene fra og med 25 til og med 29 under er en del av blokken som kjøres hvis 3 < 7 på linje 24
 
 if 3 < 7:
     print("3 er mindre enn 7")
